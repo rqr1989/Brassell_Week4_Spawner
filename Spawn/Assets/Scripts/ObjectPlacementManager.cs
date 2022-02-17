7432994c4c743be6163d8f1d7eff7d9e175699b4
rqr1989 <dontopenthepyxis@gmail.com>
2022-02-16 20:01:55 -0500

    Left Mouse Button Works
    Couldn't figure out how to get the right mouse button to work properly

--- Spawn/Assets/Scripts/ObjectPlacementManager.cs
+++ Spawn/Assets/Scripts/ObjectPlacementManager.cs
@@ -10,52 +10,44 @@ public class ObjectPlacementManager : MonoBehaviour
     public Camera cam;
     public Transform placeableObject;
     public LayerMask mask;
-    public GameObject CylinderPrefab;
+   //  public GameObject cylinderPrefab;
 
-
-
+   // public GameObject cyl;
 
     public void Awake()
     {
         cam = FindObjectOfType<Camera>();
     } 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
         //creates ray at point where mouse is, looking through camera
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
-        //
         RaycastHit hitInfo;
 
+
         //if Raycast hits something
-        if(Physics.Raycast(ray, out hitInfo,Mathf.Infinity, mask))
+        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, mask))
         {
             //place instance of object at place hit
             placeableObject.position = hitInfo.point + new Vector3(0, 0.5f, 0);
 
             //roatate on upward facing direction
             placeableObject.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
-
-            // when you left click with your mouse, the object will be placed at its location and stay there, 
             if (Input.GetButtonDown("Fire1"))
             {
 
-               // GameObject cyl;
-               // cyl = Instantiate(CylinderPrefab, transform);
-
-              
-                // Instantiate at position (0, 0, 0) and zero rotation.
-               // Instantiate(CylinderPrefab, new Vector3(0, 0.5f, 0), placeableObject.rotation);
+                // Adds cylinder
+                placeableObject = Instantiate(placeableObject, placeableObject.position, placeableObject.rotation);
             }
-            //right clicking will instantiate a new object that you can place.
-           // else if (Input.GetButtonDown("Fire2"))
-           // {
-                //CylinderPrefab
+            else if (Input.GetButtonDown("Fire2"))
+            {
 
-             
-           // }
-        
+               placeableObject= Instantiate(placeableObject);
+            }
+
+
         }
       
     }
